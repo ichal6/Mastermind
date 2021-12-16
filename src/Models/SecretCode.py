f8bf5afa9b1239e87c3d452a7e0b10a5fe54653f
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-12-16 13:36:32 +0100

    Write generate a secret code

--- src/Models/SecretCode.py
+++ src/Models/SecretCode.py
@@ -1,3 +1,12 @@
+import random
+
+
 class SecretCode:
     def __init__(self):
-        pass
+        self.__secret_code = dict()
+        for number in range(0, 4):
+            self.__secret_code[number] = random.randint(1, 6)
+
+    @property
+    def secret_code(self):
+        return self.__secret_code
