793f58e417d1274c879cc528c7dcec7a7e56590a
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-12-16 13:37:25 +0100

    Write name of methods in game rules

--- src/Models/FairGame.py
+++ src/Models/FairGame.py
@@ -1,6 +1,11 @@
 from src.Models.RegulyGry import RegulyGry
+from src.Models.SecretCode import SecretCode
 
 
 class FairGame(RegulyGry):
     def __init__(self, secret_code):
-        super(self, secret_code)
+        super().__init__(secret_code)
+    # Klasa nadrzedna posiada secret code jako pole prywatne
+    def check(self, possible_code: SecretCode) -> bool:
+        pass
+
--- src/Models/RegulyGry.py
+++ src/Models/RegulyGry.py
@@ -2,3 +2,5 @@ class RegulyGry:
     def __init__(self, secret_code):
         self.__secret_code = secret_code
 
+    def check(self, possible_code):
+        pass
