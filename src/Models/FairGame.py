d6ab44c119e0e74bdf49c2528aef6b3952fc9d67
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-12-17 14:42:18 +0100

    Implement condition for lose

--- src/Models/FairGame.py
+++ src/Models/FairGame.py
@@ -5,7 +5,7 @@ from src.Models.SecretCode import SecretCode
 class FairGame(RegulyGry):
     def __init__(self, secret_code):
         super().__init__(secret_code)
-    # Klasa nadrzedna posiada secret code jako pole prywatne
-    def check(self, possible_code: SecretCode) -> bool:
-        pass
 
+    def check(self, possible_code: SecretCode) -> bool:
+        if not self._secret_code.equal_code(possible_code.secret_code):
+            return False
--- src/Models/RegulyGry.py
+++ src/Models/RegulyGry.py
@@ -1,6 +1,9 @@
+from src.Models.SecretCode import SecretCode
+
+
 class RegulyGry:
-    def __init__(self, secret_code):
-        self.__secret_code = secret_code
+    def __init__(self, secret_code: SecretCode):
+        self._secret_code = secret_code
 
     def check(self, possible_code):
         pass
