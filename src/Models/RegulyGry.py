f77677d1dfbda85314f93ded40c170d6c3bb2ce1
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-12-17 16:54:10 +0100

    Write increase attempt number

--- src/Models/RegulyGry.py
+++ src/Models/RegulyGry.py
@@ -1,9 +1,20 @@
+from abc import ABC, abstractmethod
 from src.Models.SecretCode import SecretCode
 
 
-class RegulyGry:
+class RegulyGry(ABC):
     def __init__(self, secret_code: SecretCode):
         self._secret_code = secret_code
+        self.__attempt_number = 0
+        self.__MAX_ATTEMPT = 12
 
+    @abstractmethod
     def check(self, possible_code):
-        pass
+        raise NotImplementedError
+
+    @abstractmethod
+    def attempt(self, possible_code) -> str:
+        raise NotImplementedError
+
+    def increase_attempt_number(self):
+        self.__attempt_number += 1
