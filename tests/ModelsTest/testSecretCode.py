353f1894f2a921211724ea17e3dbb92fc75633e1
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-12-18 14:33:54 +0100

    Write tests for count_correct_position

--- tests/ModelsTest/testSecretCode.py
+++ tests/ModelsTest/testSecretCode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from src.Models.SecretCode import SecretCode
-from tests.CommonUse import generate_secret_code
+from tests.CommonUse import generate_secret_code, generate_wrong_answer
 
 
 class MainTest(unittest.TestCase):
@@ -24,22 +24,66 @@ class MainTest(unittest.TestCase):
         self.assertIsNotNone(secret_code.secret_code)
         self.assertEqual(len(secret_code.secret_code), size_of_code)
 
-    def test_3_should_return_code_as_string(self):
-        # given
-        given_code = {
+    def setUp(self) -> None:
+        self.given_code = {
             1: 6,
             2: 5,
             3: 4,
             4: 3
         }
-        code_as_string = '6543'
-        secret_code = SecretCode(given_code)
+        self.code_as_string = '6543'
+        self.secret_code = SecretCode(self.given_code)
 
+    def test_3_should_return_code_as_string(self):
+        # given
         # when
-        digits = secret_code.__str__()
+        digits = self.secret_code.__str__()
 
         # then
-        self.assertEqual(code_as_string, digits)
+        self.assertEqual(self.code_as_string, digits)
+
+    def test_4_should_return_zero_correct_positions(self):
+        # given
+        wrong_code = SecretCode(generate_wrong_answer(self.secret_code.secret_code))
+
+        # when
+        value = self.secret_code.count_correct_position(wrong_code)
+
+        # then
+        self.assertEqual(value, 0)
+
+    def test_5_should_return_three_correct_positions(self):
+        # given
+        self.given_code[1] = 1
+        wrong_code = SecretCode(self.given_code)
+
+        # when
+        value = self.secret_code.count_correct_position(wrong_code)
+
+        # then
+        self.assertEqual(3, value)
+
+    def test_6_should_return_four_correct_positions(self):
+        # given
+        wrong_code = SecretCode(self.given_code)
+
+        # when
+        value = self.secret_code.count_correct_position(wrong_code)
+
+        # then
+        self.assertEqual(4, value)
+
+    def test_7_should_return_two_correct_positions(self):
+        # given
+        self.given_code[1] = 1
+        self.given_code[2] = 2
+        wrong_code = SecretCode(self.given_code)
+
+        # when
+        value = self.secret_code.count_correct_position(wrong_code)
+
+        # then
+        self.assertEqual(2, value)
 
 
 if __name__ == '__main__':
