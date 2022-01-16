0026315965d376493de4cd6358c6fc2dfc10745d
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-01-16 13:34:27 +0100

    Add comments to test

--- tests/testMain.py
+++ tests/testMain.py
@@ -10,7 +10,14 @@ from tests.CommonUse import generate_wrong_answer, print_secret_code, mixed_up_a
 
 
 class MainTest(unittest.TestCase):
+    """
+    Główna klasa z testami
+    """
     def test_1_should_display_secret_code_and_return_error_message_if_insert_wrong_code(self):
+        """
+        Wyświetlenie (wypisanie w konsoli) wylosowanego kodu, wpisanie odpowiedzi z błędnymi cyframi
+        - oczekiwana informacja o braku poprawnych trafień.
+        """
         # given
         secret_code = SecretCode({0: 5, 1: 4, 2: 3, 3: 4})
         wrong_code_dict = generate_wrong_answer(secret_code.secret_code)
@@ -33,6 +40,10 @@ class MainTest(unittest.TestCase):
         self.assertEqual(0, number_of_correct_position)
 
     def test_2_should_display_secret_code_and_return_incorrect_position_message_if_insert_mixed_code(self):
+        """
+        Wyświetlenie wylosowanego kodu, wpisanie odpowiedzi z poprawnymi cyframi w złych miejscach
+        - oczekiwana informacja o niepoprawnym położeniu.
+        """
         # given
         secret_code = SecretCode({0: 5, 1: 4, 2: 3, 3: 4})
         game = FairGame(secret_code)
@@ -53,6 +64,10 @@ class MainTest(unittest.TestCase):
         self.assertEqual(4, number_of_incorrect_position)
 
     def test_3_should_return_two_hit_and_two_mishit(self):
+        """
+        Wyświetlenie wylosowanego kodu, wpisanie odpowiedzi z dwoma poprawnymi cyframi w dobrych miejscach i dwoma poprawnymi w złych miejscach
+        - oczekiwana informacja o dwóch trafieniach i dwóch złych pozycjach.
+        """
         # given
         secret_code = SecretCode({0: 5, 1: 4, 2: 3, 3: 4})
         game = FairGame(secret_code)
@@ -74,6 +89,11 @@ class MainTest(unittest.TestCase):
         self.assertEqual(2, number_of_correct_position)
 
     def test_4_should_win_when_insert_correct_code(self):
+        """
+        Wyświetlenie wylosowanego kodu, wpisanie poprawnej odpowiedzi
+        - oczekiwana informacja o wygranej.
+
+        """
         # given
         secret_code_dict = {0: 5, 1: 4, 2: 3, 3: 4}
         secret_code = SecretCode(secret_code_dict)
@@ -98,6 +118,9 @@ class MainTest(unittest.TestCase):
         self.assertIs(is_win, True)
 
     def test_5_should_display_game_over_message_after_12_attempts(self):
+        """
+        Wpisanie 12 razy niepoprawnego kodu - oczekiwana informacja o przegranej
+        """
         # given
         secret_code_dict = generate_secret_code()
         secret_code = SecretCode(secret_code_dict)
@@ -120,6 +143,10 @@ class MainTest(unittest.TestCase):
         self.assertEqual(12, game.attempt_number)
 
     def test_6_should_disregard_code_if_format_is_incorrect(self):
+        """
+        Próba wpisania niepoprawnego kodu do pola odpowiedzi (mniej lub więcej niż 4 znaki, znaki nie będące cyframi
+         od 1 do 6) - oczekiwane nieuznanie kodu (gracz nie traci tury).
+        """
         # given
         secret_code_dict = {0: 5, 1: 4, 2: 3, 3: 4}
         secret_code = SecretCode(secret_code_dict)
@@ -140,6 +167,10 @@ class MainTest(unittest.TestCase):
         self.assertEqual(2, game.attempt_number)
 
     def test_7_should_display_tere_fere_if_rule_is_fair(self):
+        """
+        Wciśnięcie przycisku „Oszust” przy poprawnych zasadach gry
+        - oczekiwana informacja „tere fere”.
+        """
         # given
         secret_code_dict = {0: 5, 1: 4, 2: 3, 3: 4}
         secret_code = SecretCode(secret_code_dict)
@@ -154,6 +185,10 @@ class MainTest(unittest.TestCase):
         self.assertEqual(False, is_cheater)
 
     def test_8_should_display_info_about_cheat_if_rule_is_cheat(self):
+        """
+        Wciśnięcie przycisku „Oszust” przy niepoprawnych zasadach gry
+        - oczekiwana informacja o oszukiwaniu przez komputer.
+        """
         # given
         secret_code_dict = {0: 5, 1: 4, 2: 3, 3: 4}
         secret_code = SecretCode(secret_code_dict)
@@ -168,6 +203,10 @@ class MainTest(unittest.TestCase):
         self.assertEqual(True, is_cheater)
 
     def test_9_should_can_reset_the_game(self):
+        """
+        Wpisanie 10 kodów, resetowanie gry, wpisanie 5 kodów - oczekiwane normalne działanie gry
+        (czy licznik tur resetuje się po wciśnięciu „Reset”).
+        """
         # given
         secret_code_dict = {0: 5, 1: 4, 2: 3, 3: 4}
         secret_code = SecretCode(secret_code_dict)
@@ -188,6 +227,10 @@ class MainTest(unittest.TestCase):
         self.assertEqual(5, controller._ControllerGame__game.attempt_number)
 
     def test_10_play_game(self):
+        """
+        Rozegranie próbnej gry w 6 próbach.
+        Porównanie stosownej listy nieprawidłowych pozycji do uzyskanej z programu.
+        """
         # given
         secret_code_dict = {0: 1, 1: 2, 2: 3, 3: 4}
         secret_code = SecretCode(secret_code_dict)
