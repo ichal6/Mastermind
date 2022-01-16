154caa65318a6fabde6d94e7c6d7da7cd59b3402
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-01-16 12:06:21 +0100

    Add docstrings for SecretCode

--- src/Models/SecretCode.py
+++ src/Models/SecretCode.py
@@ -4,7 +4,22 @@ from src.Validations.DataValidation import DataValidation
 
 
 class SecretCode:
+    """
+    A class used to represent a code to guess
+
+    Attributes
+    ----------
+    __secret_code: dict
+        a dictionary use to save a code to guess and code from input from user
+    """
     def __init__(self, secret_code: dict = None):
+        """
+        Parameters
+        ----------
+        secret_code: dict
+            dict with value of secret code, if we would like a create class with
+            set dict (no random)
+        """
         positions = [p for p in range(0, 4)]
         if secret_code is None:
             self.__secret_code = {position: random.randint(1, 6) for position in positions}
@@ -13,6 +28,9 @@ class SecretCode:
             DataValidation.validate_secret_code_dict(secret_code)
 
     def __str__(self):
+        """
+        Generate visual code for user
+        """
         digits = ''
         for digit in self.secret_code.values():
             digits += f'{digit}'
@@ -23,12 +41,19 @@ class SecretCode:
         return self.__secret_code
 
     def equal_code(self, other):
+        """
+        This method compare two object of code. If it's the same return True.
+        If not return False
+        """
         if self.__secret_code == other.secret_code:
             return True
         else:
             return False
 
     def count_correct_position(self, other):
+        """
+        This method return count of the same position, compare to other object.
+        """
         count = 0
         for key, value in other.secret_code.items():
             if self.__secret_code[key] == value:
@@ -36,6 +61,10 @@ class SecretCode:
         return count
 
     def count_incorrect_position(self, other):
+        """
+        This method return count of the number with incorrect position,
+        compare to other object.
+        """
         count = 0
         print(self.__str__())
         correct_list = list(self.__secret_code.values())
